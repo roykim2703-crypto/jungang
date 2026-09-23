18c960537def2e4b4b00bd4676cf07fe50aadccc
munz-cpu <munzhoo12@gmail.com>
2026-09-24 00:50:21 +1000

    육목 규칙 추가

--- Assets/문지후/scripts/수_놓기.cs
+++ Assets/문지후/scripts/수_놓기.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class 수_놓기 : MonoBehaviour
@@ -16,9 +16,23 @@ public class 수_놓기 : MonoBehaviour
 
     [SerializeField] 가이드_돌_코드 guideStoneScript;
 
+    private readonly 육목_규칙 규칙 = new 육목_규칙();
+    private bool 증강사용중;
+
+    public int 획득한증강수 => 규칙.획득한_증강_수;
+
     void Start()
     {
+        isBlack = true;
+        규칙.증강_획득 += 증강획득알림;
+        guideStoneScript.돌놓기완료 += 턴종료;
         이제너의턴();
+    }
+
+    void OnDestroy()
+    {
+        규칙.증강_획득 -= 증강획득알림;
+        if (guideStoneScript != null) guideStoneScript.돌놓기완료 -= 턴종료;
     }
     public void 이제너의턴()
     {
@@ -26,10 +40,37 @@ public class 수_놓기 : MonoBehaviour
         guideStoneScript.centerPos = centerPos;
         guideStoneScript.gap = gap;
         guideStoneScript.isBlack = isBlack;
+        guideStoneScript.증강사용중 = 증강사용중;
+        guideStoneScript.규칙 = 규칙;
         
         
         guideStoneScript.격자로놓기시작();
         
+    }
+
+    /// <summary>돌을 두기 전, 현재 턴에 증강을 쓸지 지정한다.</summary>
+    public void 증강사용설정(bool 사용)
+    {
+        증강사용중 = 사용;
+        guideStoneScript.증강사용중 = 사용;
+    }
+
+    private void 증강획득알림(int 보유수)
+    {
+        Debug.Log($"증강 획득! 현재 획득 수: {보유수}");
+    }
+
+    private void 턴종료()
+    {
+        isBlack = !isBlack;
+        증강사용중 = false;
+        StartCoroutine(다음턴시작());
+    }
+
+    private IEnumerator 다음턴시작()
+    {
+        yield return null;
+        이제너의턴();
     }
 
     public void 너흑()
